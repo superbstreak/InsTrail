0e35472c6dbd16779dbab5ec00836c27cc67f4ea
semihsezer <semih.c.sezer@gmail.com>
2016-02-28 02:17:11 -0800

    logged in user's image added to instagram auth

--- InsTrail/app/src/main/java/nwhack/instrail/com/instrail/Account.java
+++ InsTrail/app/src/main/java/nwhack/instrail/com/instrail/Account.java
@@ -1,21 +1,25 @@
 package nwhack.instrail.com.instrail;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 
 import net.londatiga.android.instagram.Instagram;
 import net.londatiga.android.instagram.InstagramUser;
 
 public class Account extends MainActivity {
-
-    WebView webView;
 
     private ProgressBar mLoadingPb;
     private GridView mGridView;
@@ -45,11 +49,30 @@ public class Account extends MainActivity {
 
                 InstagramUser instagramUser = mInstagramSession.getUser();
 
-                mLoadingPb = (ProgressBar) findViewById(R.id.pb_loading);
+                //mLoadingPb = (ProgressBar) findViewById(R.id.pb_loading);
                 mGridView = (GridView) findViewById(R.id.gridView);
+
 
                 ((TextView) findViewById(R.id.tv_name)).setText(instagramUser.fullName);
                 ((TextView) findViewById(R.id.tv_username)).setText(instagramUser.username);
+
+
+                final ImageView photo = (ImageView) findViewById(R.id.iv_user);
+
+                ImageRequest request = new ImageRequest(instagramUser.profilPicture,
+                        new Response.Listener<Bitmap>() {
+                            @Override
+                            public void onResponse(Bitmap bitmap) {
+                                photo.setImageBitmap(bitmap);
+                            }
+                        }, 0, 0, null,
+                        new Response.ErrorListener() {
+                            public void onErrorResponse(VolleyError error) {
+                                photo.setImageResource(R.drawable.ic_photo);
+                            }
+                        });
+
+                MainActivity.getVolleyController().addToRequestQueue(request);
 
 
                 ((Button) findViewById(R.id.btn_logout)).setOnClickListener(new View.OnClickListener() {
@@ -78,12 +101,13 @@ public class Account extends MainActivity {
 
         @Override
         public void onError(String error) {
+
             showToast(error);
         }
 
         @Override
         public void onCancel() {
-            showToast("OK. Maybe later?");
+
         }
 
     };
