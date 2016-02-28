a13ee9fc422d2410ff2824cb04474935911c5aa4
Rob Wu <robwu15@gmail.com>
2016-02-28 01:04:41 -0800

    MM account.java

--- InsTrail/app/src/main/java/nwhack/instrail/com/instrail/Account.java
+++ InsTrail/app/src/main/java/nwhack/instrail/com/instrail/Account.java
@@ -1,37 +1,50 @@
 package nwhack.instrail.com.instrail;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import net.londatiga.android.instagram.Instagram;
+import net.londatiga.android.instagram.InstagramRequest;
+import net.londatiga.android.instagram.InstagramSession;
 import net.londatiga.android.instagram.InstagramUser;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Account extends MainActivity {
 
     WebView webView;
 
+    private InstagramSession mInstagramSession;
+    private Instagram mInstagram;
+
     private ProgressBar mLoadingPb;
     private GridView mGridView;
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        mInstagramSession = mInstagram.getSession();
-
-        //setContentView(R.layout.activity_account);
-        mInstagram.authorize(mAuthListener);
-
-
-    }
+    private static final String CLIENT_ID = "d91dcfac9ed346478e76999806a15b59";
+    private static final String CLIENT_SECRET = "cc8e2069c8c64e29900060d94475b71d";
+    private static final String REDIRECT_URI = "com-instrail://instagramredirect";
 
     private Instagram.InstagramAuthListener mAuthListener = new Instagram.InstagramAuthListener() {
         @Override
@@ -46,8 +59,8 @@ public class Account extends MainActivity {
                 showToast("Already Logged in!");
                 InstagramUser instagramUser = mInstagramSession.getUser();
 
-                mLoadingPb = (ProgressBar) findViewById(R.id.pb_loading);
-                mGridView = (GridView) findViewById(R.id.gridView);
+                mLoadingPb 	= (ProgressBar) findViewById(R.id.pb_loading);
+                mGridView	= (GridView) findViewById(R.id.gridView);
 
                 ((TextView) findViewById(R.id.tv_name)).setText(instagramUser.fullName);
                 ((TextView) findViewById(R.id.tv_username)).setText(instagramUser.username);
@@ -90,9 +103,23 @@ public class Account extends MainActivity {
     };
 
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mInstagram = new Instagram(this, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+        mInstagramSession   = mInstagram.getSession();
+
+        //setContentView(R.layout.activity_account);
+        mInstagram.authorize(mAuthListener);
+
+
+    }
+
     private void showToast(String text) {
         Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
     }
+
 
 
 }
